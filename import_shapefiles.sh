339bd6fc4575c3fc9f2be47932117c5b880df633
Nicola Jordan <nic@hixi.ch>
2016-06-17 11:07:36 +0200

    use better named tables

--- import_shapefiles.sh
+++ import_shapefiles.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+source /root/table_names
+
 /root/wait-for-it.sh ${POSTGRES_HOST}:${POSTGRES_PORT}
 
-COASTLINES=/data/osmboundaries/coastlines.shp
-WATER=/data/osmboundaries/water_polygons.shp
-LAND=/data/osmboundaries/land_polygons.shp
+COASTLINES=/data/osmboundaries/${COASTLINE_NAME}.shp
+WATER=/data/osmboundaries/${WATER_NAME}.shp
+LAND=/data/osmboundaries/${LANDMASS_NAME}.shp
 
 ogr2ogr_command(){
   ogr2ogr --config PG_USE_COPY YES \
@@ -14,17 +16,17 @@ ogr2ogr_command(){
     -overwrite "$1"
 }
 
-echo "importing coastlines"
+echo "importing coastlines into ${COASTLINE_NAME}"
 ogr2ogr_command ${COASTLINES}
 echo "coastlines imported"
 
 echo ""
-echo "importing water"
+echo "importing water into ${WATER_NAME}"
 ogr2ogr_command ${WATER}
 echo "water imported"
 
 echo ""
-echo "importing land"
+echo "importing land into ${LANDMASS_NAME}"
 ogr2ogr_command ${LAND}
 echo "land imported"
 
